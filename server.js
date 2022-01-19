532127423a9801e52667ca400722114a75a87374
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-19 13:05:00 -0500

    Added npm,.env,readme,server.js,app.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv'); //requires the dotenv library
+const app = require('./app'); //requires the information in the app file
+
+dotenv.config();
+const PORT = process.env.PORT
+
+app.listen(PORT,() => {
+    console.log(`Listening to PORT: ${PORT}` )
+})
