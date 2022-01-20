9ec0c03a66fe28f3264526c0827e805b8cf5e6b0
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 06:53:31 -0500

    Removed ENV from being tracked in github

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use((cors()));
 app.use((express.json()));
 
 app.get('/',(request,response) => {
-    response.send("Welcone to the budgeting app")
+    response.send("Welcome to the budgeting app")
 })
 
 
