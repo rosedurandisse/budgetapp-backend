fadcf531076d70a750ce4c2b7d98ebf545bc264e
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 07:42:39 -0500

    Updating to include budgeting controllers file

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use((cors()));
-app.use((express.json()));
+const budgetingControllers = require("./controllers/budgetingControllers");
 
-app.get('/',(request,response) => {
-    response.send("Welcome to the budgeting app")
-})
+app.use(cors());
+app.use(express.json());
+app.use("/transactions", budgetingControllers);
 
+app.get("/", (request, response) => {
+  response.send("Welcome to the budgeting app");
+});
 
-app.get((request,response) => {
-    response.status(404)
-    .json({error:'Page not found'})
-})
+app.get((request, response) => {
+  response.status(404).json({ error: "Page not found" });
+});
 
 module.exports = app;
