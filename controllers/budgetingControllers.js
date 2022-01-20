d30e30ee76cad93f2037fb719158c62aaf30a3f4
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 08:03:17 -0500

    Added delete, post, and put in the controllers file

--- controllers/budgetingControllers.js
+++ controllers/budgetingControllers.js
@@ -1,22 +1,37 @@
-const express = require('express');
-const transactionArray = require('../models/transactionPlacebo');
+const { request } = require("express");
+const express = require("express");
+const transactionArray = require("../models/transactionPlacebo");
 const transactions = express.Router();
 
-
 //get the page route /transactions results in the array
-transactions.get('/', (request,response) => {
-    console.log("This is the home page showing the array")
-    response.send(transactionArray)
-})
+transactions.get("/", (request, response) => {
+  console.log("This is the home page showing the array");
+  response.send(transactionArray);
+});
 
-//
-transactions.get('/:index', (request,response) => {
-    const {index} = request.params;
-    console.log("Get an index")
-    if (transactionArray[index]) {
-        response.json(transactionArray[index])
-    }
-})
+//destructing index so necessary to put in
+transactions.get("/:index", (request, response) => {
+  const { index } = request.params;
+  console.log("Get an index");
+  transactionArray[index]
+    ? response.json(transactionArray[index])
+    : response.redirect("/transactions", 302);
+});
 
+transactions.delete("/:index", (request, response) => {
+  const { index } = request.params;
+  transactionArray.splice(index, 1);
+  response.json(transactionArray);
+});
 
+transactions.post("/", (request, response) => {
+  transactionArray.push(request.body);
+  response.status(201).json(transactionArray);
+});
+
+transactions.put("/index", (request, response) => {
+  const { index } = request.params;
+  transactionArray[index] = request.body;
+  response.status(201).json(transactionArray);
+});
 module.exports = transactions;
