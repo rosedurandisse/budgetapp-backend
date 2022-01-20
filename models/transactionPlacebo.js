a2cf170ff008e2ac322958826c9cfa50458dbe11
Rose Durandisse <rosedurandisse@pursuit.org>
2022-01-20 07:47:36 -0500

    Expanded the array to include more transactions

---
+++ models/transactionPlacebo.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    item_name: "retirement",
+    amount: -300,
+    date: "December 20,2021",
+    from: "employer",
+    category: "bank",
+  },
+  {
+    item_name: "savings",
+    amount: -100,
+    date: "December 26,2021",
+    from: "transfer",
+    category: "bank",
+  },
+  {
+    item_name: "income",
+    amount: 1000,
+    date: "August 26,2021",
+    from: "employer",
+    category: "payment",
+  },
+  {
+    item_name: "taxes",
+    amount: -40,
+    date: "August 26,2022",
+    from: "confusion",
+    category: "bank",
+  },
+
+  //     item_name- string - the name of the transaction (ie: income, savings, cat food, etc.)
+  // amount -number - the amount of the transaction
+  // date- string - the date should be a simple string. As a bonus activity, use the date object and date input field and format it to be human-readable
+  // from - string - who this transacton was with (ie. employer, bank, pet store, grocery store, etc)
+  // categor
+];
